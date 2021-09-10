b29534c57283ec6f15467855c78221b203fba3ff
Antarpreet <lucky0020@gmail.com>
2021-09-10 09:39:21 +0530

    commit for deployment

--- taggar/settings.py
+++ taggar/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(strtobool(os.environ['DEBUG']))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['taggarassociates.com', 'www.taggarassociates.com', '127.0.0.1']
 
 
 # Application definition
